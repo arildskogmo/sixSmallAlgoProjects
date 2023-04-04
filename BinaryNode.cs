25d15bb986d3c3c39d815534b358f8ede937a7b8
Skoggy <arild.skogmo@hotmail.com>
2023-04-04 14:25:01 +0200

    Add files via upload

---
+++ BinaryNode.cs
@@ -0,0 +1,31 @@
+﻿
+namespace binary_node1
+{
+    public class BinaryNode<T>
+    {
+        public BinaryNode(T value)
+        {
+            Value = value;
+            LeftChild = null;
+            RightChild = null;
+        }
+        
+        public T Value { get; set; }
+
+        public BinaryNode<T>? LeftChild { get; set; }
+        public BinaryNode<T>? RightChild { get; set; }
+
+        public void AddLeft(BinaryNode<T> node)
+        {
+            LeftChild = node;
+        }
+        public void AddRight(BinaryNode<T> node)
+        {
+            RightChild = node;
+        }
+        public override string ToString()
+        {
+            return Value?.ToString() + ": " + (LeftChild?.Value?.ToString() ?? "null") + " " + (RightChild?.Value?.ToString() ?? "null");
+        }
+    }
+}
---
+++ NaryNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_node1
+{
+    public class NaryNode<T>
+    {        
+        public NaryNode(T value) 
+        { 
+            Value = value;
+            Children = new List<NaryNode<T>>();
+        }
+
+        public T Value { get; set; }
+
+        public List<NaryNode<T>> Children { get; set; }
+
+        public void AddChild(NaryNode<T> node)
+        {
+            Children.Add(node);
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString() + ": " + string.Join(" ", Children.Select(x => x.Value));
+        }
+    }
+}
